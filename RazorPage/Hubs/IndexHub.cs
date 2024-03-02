d16b4d2410fb713b13bd40f22087d067f0054b2e
Pete <pisces72297@gmail.com>
2024-03-02 18:41:37 +0800

    modify import model

--- RazorPage/Hubs/IndexHub.cs
+++ RazorPage/Hubs/IndexHub.cs
@@ -19,12 +19,12 @@ namespace RazorPage.Hubs
         }
         public override async Task OnConnectedAsync()
         {
-            _logger.LogInformation("GroupHub.OnConnectedAsync:{v}", Context.GetHashCode());
+            _logger.LogInformation("IndexHub.OnConnectedAsync:{v}", Context.GetHashCode());
             await base.OnConnectedAsync();
         }
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
-            _logger.LogInformation("GroupHub.OnDisconnectedAsync:{v}", Context.GetHashCode());
+            _logger.LogInformation("IndexHub.OnDisconnectedAsync:{v}", Context.GetHashCode());
             await base.OnDisconnectedAsync(exception);
         }
     }
